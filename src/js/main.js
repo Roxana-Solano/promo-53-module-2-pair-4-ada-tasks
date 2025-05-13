a52a150052d7e69140cbd571c27e8a9e7234d31c
Roxana Solano <roxansolan@gmail.com>
2025-05-13 20:26:30 +0200

    Server requests

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,6 @@ function eventClick(e) {
 } //target el que hace click, el de dentro // el currentTarget el de fuera al que se le agrega la clase
 function renderTasks() {
   list.innerHTML = "";
-
   for (const task of tasks) {
     list.insertAdjacentHTML(
       "beforeend",
@@ -25,35 +24,24 @@ function renderTasks() {
     const check = document.querySelector(`#id-${task.id}`);
     check.addEventListener("change", eventClick);
   }
-  updateTaskCounter();
 }
-fetch(SERVER_URL)
+
+function request() {
   /* {
-    method: "GET", 
-    }  */
-
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    tasks = data.results;
-    renderTasks();
-
-    console.log(data);
-  });
-
-function createTaskCounter() {
-  // Revisamos si ya existe el elemento, para no duplicarlo
-  if (!document.querySelector(".task-counter")) {
-    const container = document.querySelector(".task-list").parentElement;
-    const counterElement = document.createElement("p");
-    counterElement.classList.add("task-counter");
-    container.insertBefore(
-      counterElement,
-      container.querySelector(".task-list")
-    );
-  }
+      method: "GET", 
+      }  */
+  fetch(SERVER_URL)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      tasks = data.results;
+      renderTasks();
+      console.log(data);
+    });
 }
+
+request();
 
 //Completa el código;
 //Guarda la respuesta obtenida enla variable para el listado de tareas: `tasks`
